3eb83489bd66f38db79c84f959a14240ed9e1eb8
john <jdqwertqwert@gmail.com>
2018-04-21 01:26:49 +0200

    finished startscreen except for team colors

--- trends_game/components/components.js
+++ trends_game/components/components.js
@@ -1,5 +1,5 @@
 
-function Comps() {
+function Startscreen_components() {
     this.get_alert_danger = function (msg) {
         return this.get_alert(msg, "alert-danger")
     };
@@ -22,32 +22,32 @@ function Comps() {
 
     this.get_add_team_row = function(team_num){
         return `<div class="row" id="team_row_${team_num}" style="height: 25px;margin-bottom: 6px;">
-                <div class="col-10" style="padding-right: 0;">
-                    <label class="pull-left" style="
+                  <div class="col-10" style="padding-right: 0;">
+                    <label class="pull-left team_name_label" style="
                            margin-top: 2px;
                            background-color: slateblue;
                            border-radius:5px;padding-left: 5px;
                            padding-right: 5px;
                            width: 100%;
-                           text-align: center;">
-                        Team ${+team_num + 1}</label>
+                           text-align: center;"
+                           >${startscreen_consts.default_teamname}</label>
                     <input class="clickedit" id="team_name_${team_num}" type="text" />
-                </div>
-                <div class="col-2" style="padding-right: 30px; padding-left: 0px;">
-                    <button type="button" class="btn btn-danger btn-block" id="team_del_${team_num}"
-                            style="
-                            text-align: center;
-                            padding: 0rem  12px;"
-                    >X</button>
-                </div>
-                <script>hide_inputs()</script>            
+                  </div>
+                  <div class="col-2" style="padding-right: 30px; padding-left: 0px;">
+                      <button type="button" class="btn btn-danger btn-block" id="team_del_${team_num}"
+                              style="
+                              text-align: center;
+                              padding: 0rem  12px;"
+                      >X</button>
+                  </div>
+                  <script>hide_inputs()</script>            
                 </div>`
     };
-
 
     this.get_clearfix = function () {
         return `<div class="clearfix"></div>`
     };
+
 }
 
 
--- trends_game/game.js
+++ trends_game/game.js
@@ -1,32 +1,38 @@
-
 let gamediv = $("#game");
 let startscreen_html = httpGet("/trends_game/components/start_screen.html");
 let gamescreen_html = httpGet("/trends_game/components/game_screen.html");
-let comps = new Comps();
+let comps = new Startscreen_components();
 
+const startscreen_consts = {
+    default_teamname: "Click to edit teamname"
+};
 
 function start_screen() {
     gamediv.html(startscreen_html);
     let message_div = $("#message_div");
     let teams_div = $("#teams");
-    let add_btn = $("#add_team");
-
+    let add_team_btn = $("#add_team");
+    let start_game_btn = $("#start_game");
     let team_id_counter = 0;
     let max_teams = 4;
 
-    add_btn.click((event) => {
+    //todo team colors
+
+    function add_team(){
         if(teams_div.children().length < max_teams){
             teams_div.append(comps.get_add_team_row(team_id_counter));
             team_id_counter++;
         }else{
             message_div.html(comps.get_alert_info("You can only have up to "+max_teams+" teams."))
         }
-    });
+    }
+
+    add_team_btn.click((event) => {add_team()});
+    add_team();
 
     teams_div.click((event) => {
         let id = event.originalEvent.target.id;
         let team_del = "team_del_";
-
 
         if(id.indexOf(team_del) === 0) {
             let id_num = id.replace(team_del, "");
@@ -34,6 +40,44 @@ function start_screen() {
         }
     });
 
+
+    start_game_btn.click((event) => {
+        let teams = [];
+        let team_names_labels = $("label.team_name_label").toArray();
+
+        function Team(num, name){
+            this.num = num;
+            this.name = name
+        }
+
+        let _continue = true;
+        team_names_labels.forEach((label) => {
+            if (_continue){
+                let team_num = label.parentElement.parentElement.id.replace("team_row_", "");
+                let team_name = label.textContent;
+                if (team_name === startscreen_consts.default_teamname ){
+                    _continue = false;
+                }else{
+                    teams.push(new Team(team_num, team_name))
+                }
+            }
+        });
+        // right now 2 teams can have the same name since they're identified by their number
+        if (_continue){
+            game_screen(teams)
+        }else{
+            message_div.html(comps.get_alert_warning("All teams must set their name before starting the game."));
+        }
+    });
+
+}
+
+function game_screen(teams){
+    gamediv.html(gamescreen_html);
+    teams.forEach((team) => {
+        gamediv.append(`<span>${team.num} ${team.name}</span>`);
+        gamediv.append(comps.get_clearfix());
+    });
 }
 
 start_screen();
