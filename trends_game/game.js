9d09f597e6205d4e5f862b7bb8c9dc8c6ca19d7a
john <jdqwertqwert@gmail.com>
2018-05-02 01:16:37 +0200

    added autoresizing table on gamscreen.

--- trends_game/game.js
+++ trends_game/game.js
@@ -1,36 +1,25 @@
 
-const gamediv = $("#game");
-
-let startscreen_html;
 let startscreen_templ;
-let gamescreen_html;
+let game_templ;
 r_async.parallel([
-    () => {startscreen_html = httpGet("/trends_game/components/start_screen.html")},
     () => {startscreen_templ = httpGet("/trends_game/components/start_screen.vueable")},
-    () => {gamescreen_html = httpGet("/trends_game/components/game_screen.html")}
+    () => {game_templ = httpGet("/trends_game/components/game_screen.vueable")}
+
 ]);
 let parser = new Vueable();
+let startscreen_active = false;
 
-const startscreen_consts = {
-    default_teamname: "Click to edit teamname"
-};
-
-// console.log(startscreen_templ);
-let parsed = parser.parse(startscreen_templ);
-// console.log(parsed);
-
-Vue.component('start_screen', {
-    template: parsed,
+/*Vue.component('start_screen', {
+    template:  parser.parse(startscreen_templ),
     data: function () {
         return {
             teams: [],
-            default_teamname: startscreen_consts.default_teamname,
+            default_teamname: "Click to edit teamname",
             t_input_id: "t_input_",
             max_teams: 4,
             team_id_counter: 0,
-            active: true,
+            active: startscreen_active,
             alert_templ: new Global_Comps()
-
         }
     },
     mounted: function(){
@@ -41,38 +30,9 @@ Vue.component('start_screen', {
             if (this.teams.length >= this.max_teams){
                 this.show_msg(this.alert_templ.get_alert_info("You can only have up to "+this.max_teams+" teams."))
             }else{
-                this.teams.push(this.team_id_counter);
+                this.teams.push(
+                    this.get_new_team(this.team_id_counter));
                 this.team_id_counter++;
-            }
-        },
-        start_game: function() {
-            //todo refactor this
-            let teams = [];
-            let team_names_labels = $("label.team_name_label").toArray();
-
-            function Team(num, name){
-                this.num = num;
-                this.name = name
-            }
-
-            let _continue = true;
-            team_names_labels.forEach((label) => {
-                if (_continue){
-                    let team_num = label.parentElement.parentElement.id.replace("team_row_", "");
-                    let team_name = label.textContent;
-                    if (team_name === startscreen_consts.default_teamname ){
-                        _continue = false;
-                    }else{
-                        teams.push(new Team(team_num, team_name))
-                    }
-                }
-            });
-            // right now 2 teams can have the same name since they're identified by their number
-            if (_continue){
-                game_screen(teams);
-                this.active = false;
-            }else{
-                this.show_msg(this.alert_templ.get_alert_warning("All teams must set their name before starting the game."));
             }
         },
         update_teamname: _.debounce(function(event){
@@ -97,9 +57,9 @@ Vue.component('start_screen', {
         }, 200),
         delete_team: function(team_num){
             let _t = this.teams;
-            let team_idx = _t.indexOf(+ team_num);
+            let team_idx = _t.map((e) => {return e.num}).indexOf(+ team_num);
             if (team_idx > -1){
-                _t.splice(_t.splice(team_idx, 1))
+                _t.splice(team_idx, 1)
             }else{
                 this.show_msg(this.alert_templ.get_alert_warning("Can't delete team, doesn't seem to exist."))
             }
@@ -108,18 +68,136 @@ Vue.component('start_screen', {
             $("#message_div").html(msg_html)
         },
         get_new_team: function(team_num) {
-            return {
+            let ret = {
                 num: team_num,
                 name: this.default_teamname
+            };
+            return ret;
+        },
+        start_game: function() {
+            let found = [];
+            let game_is_ready = true;
+
+            this.teams.map((e) => {return e.name}).forEach((name) => {
+                if (game_is_ready){
+                    if (name === this.default_teamname ){
+                        game_is_ready = false;
+                        this.show_msg(this.alert_templ.get_alert_info("All teams need have changed their name before starting."));
+                    }else if(found.indexOf(name) !== -1){
+                        game_is_ready = false;
+                        this.show_msg(this.alert_templ.get_alert_info("All teamnames need to be unique."));
+                    }else{
+                        found.push(name);
+                    }
+                }
+            });
+            if (game_is_ready){
+                console.log("starting game");
+                this.teams.forEach((e) => {
+                    console.log(e.name + e.num);
+                });
+                this.active = false;
             }
         }
     }
-});
+});*/
+
+const canvas_cntx = document.createElement("canvas").getContext("2d");
+function get_max_text_width (strings, font) {
+    canvas_cntx.font = font;
+    let max_width = 0;
+    strings.forEach((str) => {
+        let width = canvas_cntx.measureText(str).width;
+        if (width > max_width){
+            max_width = width;
+        }
+    });
+    return max_width;
+}
+
+Vue.component("game",{
+        //putting this in a table
+        template: parser.parse(game_templ),
+        data: function () {
+            let teams = [{
+                name: "t name 1 blaaaah",
+                num: 3
+            },{
+                name: "t name 2",
+                num: 5
+            },{
+                name: "test",
+                num: 6
+            }];
+            const table_header_font = "bold 12pt Montserrat";
+            const points_span_font = "italic 12pt Montserrat";
+            const query_text_font = "12pt Montserrat";
+
+            let min_col_width = get_max_text_width(teams.map((t) => {return t.name}) , table_header_font) + 20;
+            let fnt = table_header_font;
+            //let test_elem = $(`<div style="display: inline-block"><span style="font:${fnt};width:${get_max_text_width([txt], fnt)}px;float: left;padding-top: 3px;">${txt}</span><span style="font: ${query_text_font}">query_text</span></div>`);
+
+            let points_span_width = get_max_text_width(["100 points: "], fnt);
+
+
+            return {
+                active: true,
+                teams: teams,
+                table_header_font:table_header_font,
+                points_span_font:points_span_font,
+                query_text_font: query_text_font,
+                points_span_width: points_span_width,
+                queries: [],
+                min_col_width: min_col_width
+                /* create a 2d array for the query results,
+                 * which the x which matches a team array index,
+                 * and the y matches a round (aka a rou)*/
+            }
+        },
+        mounted: function(){
+            this.add_query_row([ this.get_new_query_item(100, "test"), this.get_new_query_item(43, "testing"), this.get_new_query_item(43, "testing")]);
+            this.add_query_row([ this.get_new_query_item(52, "boooooooh"), this.get_new_query_item(61, "bleh"), this.get_new_query_item(43, "testing")]);
+            this.add_query_row([ this.get_new_query_item(52, "boooooooh"), this.get_new_query_item(61, "bleh"), this.get_new_query_item(43, "testing")]);
+
+        },
+        computed:{
+        },
+        methods:{
+            add_query_row: function(row) {
+                let w = this.min_col_width;
+                row.forEach((e) => {
+                    if (e.width > w){
+                        w = e.width;
+                        console.log("updated width");
+                    }
+                });
+                this.min_col_width = w;
+                this.queries.push(row)
+            },
+
+            get_new_query_item: function (points, _term) {
+                let width = this.points_span_width + get_max_text_width([_term], this.query_text_font);
+                return{
+                    points_txt : points+" points:  ",
+                    term: _term,
+                    width: width
+                }
+            },
+            test: function() {
+                this.min_col_width += 10;
+            }
+        }
+    }
+);
+
 
 // bootstrap the demo
 let demo = new Vue({
     el: '#demo'
 });
+
+const gamediv = $("#game");
+
 
 const gamescreen_consts = {
 
